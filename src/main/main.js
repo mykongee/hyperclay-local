f9d6c6f7ca9f70e3f323dfc6773f04b513d5c148
mykongee <mikehongde@gmail.com>
2026-02-18 03:38:16 -0500

    chore: release v1.8.1

--- src/main/main.js
+++ src/main/main.js
@@ -27,8 +27,8 @@ if (process.platform === 'darwin') {
   const iconPath = path.join(__dirname, '../../assets/icons/icon.png');
   const aboutOptions = {
     applicationName: 'Hyperclay Local',
-    applicationVersion: '1.8.0',
-    version: '1.8.0',
+    applicationVersion: '1.8.1',
+    version: '1.8.1',
     copyright: 'Made with ❤️ for Hyperclay'
   };
 
